774778236ffe2380bf38c13094864d23064d14eb
Arjun Guha <arjun@cs.brown.edu>
2008-11-16 14:28:12 -0500

    trailing slash unnecessary

--- website/persistence/urls.py
+++ website/persistence/urls.py
@@ -3,7 +3,9 @@ from django.conf import settings
 
 urlpatterns = patterns('',
   (r'^fxserver/getobj/(?P<obj_id>\w+)/$','fxserver.views.getobj'),
+  (r'^fxserver/getobj/(?P<obj_id>\w+)$','fxserver.views.getobj'),
   (r'^fxserver/setobj/(?P<obj_id>\w+)/$','fxserver.views.setobj'),
+  (r'^fxserver/setobj/(?P<obj_id>\w+)$','fxserver.views.setobj'),
 
 #     (r'^admin/', include('django.contrib.admin.urls')),
 )
