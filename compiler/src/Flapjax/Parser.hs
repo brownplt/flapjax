5a7cc79ecd600fbb36ed95cd671cc180d089cc96
Arjun Guha <arjun@cs.brown.edu>
2009-03-25 22:30:50 -0400

    omit leading whitespace and comments

--- compiler/src/Flapjax/Parser.hs
+++ compiler/src/Flapjax/Parser.hs
@@ -3,15 +3,16 @@ module Flapjax.Parser(parseScript,parseInline,parseInlineAttribute) where
 import Control.Monad
 import Text.ParserCombinators.Parsec
 import WebBits.JavaScript.Parser (parseStatement,parseExpression)
+import WebBits.JavaScript.Lexer (whiteSpace)
 import Flapjax.Syntax
 
 parseScript =
-  spaces >> liftM2 FlapjaxScript getPosition (parseStatement `sepBy` spaces)
+  whiteSpace >> liftM2 FlapjaxScript getPosition (parseStatement `sepBy` spaces)
 
 parseInline =
-  spaces >> (liftM2 Inline getPosition parseExpression <?> 
+  whiteSpace >> (liftM2 Inline getPosition parseExpression <?> 
              "expression within {! ... !}")
 
 parseInlineAttribute =
-  spaces >> (liftM2 InlineAttribute getPosition parseExpression <?>
+  whiteSpace >> (liftM2 InlineAttribute getPosition parseExpression <?>
              "expression without {! ... !} in an attribute")
