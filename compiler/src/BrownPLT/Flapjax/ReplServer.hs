4d6628655b4cbbafce09c252e749dfb840cb86a3
Arjun Guha <arjun@cs.brown.edu>
2009-10-20 18:06:55 -0400

    bugfix

--- compiler/src/BrownPLT/Flapjax/ReplServer.hs
+++ compiler/src/BrownPLT/Flapjax/ReplServer.hs
@@ -15,6 +15,10 @@ import Network.HTTP
 import Network.HTTP.Server
 import Network.URL
 import Network.URI
+import System.Directory
+import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+import Data.ByteString.Internal (c2w, w2c)
 
 parseExpr = do
   whiteSpace
@@ -23,32 +27,39 @@ parseExpr = do
   return e
 
 
-ok :: String -> Response String
+ok :: ByteString -> Response ByteString
 ok str = 
   Response (2,0,0) "ok" 
            [ Header HdrCacheControl "no-store, must-revalidate"
-           , Header HdrContentLength (show $ length str)
+           , Header HdrContentLength (show $ BS.length str)
            ]
            str
 
+four04 = Response (4,0,4) "file not found"
+           [ Header HdrCacheControl "no-store, must-revalidate"
+           , Header HdrContentLength "0"
+           ]
+           BS.empty
 
-compileExprService req = case (rqBody req) of
-  'e':'x':'p':'r':'=':src -> do -- total disaster
+
+compileExprService req = case (map w2c $ BS.unpack $ rqBody req) of
+  'e':'x':'p':'r':'=':escSrc -> do -- total disaster
+    let src = urlDecode escSrc
     putStrLn $ "Compiling: "  ++ src
     case parse parseExpr "web request" src of
-      Left _ -> return $ ok "throw \'parse error\'"
+      Left _ -> return $ ok (BS.pack $ map c2w "throw \'parse error\'")
       Right fxExpr -> do
         jsExpr <- compileExpr defaults fxExpr
-        return $ ok (renderExpression jsExpr)
+        return $ ok (BS.pack (map c2w $ renderExpression jsExpr))
   _ -> do
     putStrLn "compile: no expression provided"
-    return $ ok "throw \'expression not provided\'"
+    return $ ok (BS.pack (map c2w "throw \'expression not provided\'"))
 
 
 flapjaxREPLServer :: Int -> FilePath -> IO ()
 flapjaxREPLServer port rootPath = server (handler rootPath)
 
-handler :: FilePath -> Handler String
+handler :: FilePath -> Handler ByteString
 handler rootPath sockAddr url request = do
   let path = uriPath (rqURI request)
   putStrLn path
@@ -56,8 +67,13 @@ handler rootPath sockAddr url request = do
     "/compile" -> compileExprService request
     "/ping" -> do
       putStrLn "Responding to a ping."
-      return (ok "pong")
+      return (ok $ BS.pack $ map c2w "pong")
     otherwise -> do
-      
-      bytes <- readFile (rootPath ++ path)
-      return (ok bytes)
+      let filePath = rootPath ++ path
+      exists <- doesFileExist filePath
+      case exists of
+        True -> do   
+          bytes <- BS.readFile filePath
+          putStrLn $ "Read a file of length " ++ show (BS.length bytes)
+          return (ok bytes)
+        False -> return four04
