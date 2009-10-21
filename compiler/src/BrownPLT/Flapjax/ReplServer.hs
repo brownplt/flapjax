33dd9e87e49cab3dc303298f7c66bf5f1d2eda7a
Arjun Guha <arjun@cs.brown.edu>
2009-10-21 10:45:15 -0400

    moving to libcurl

--- compiler/src/BrownPLT/Flapjax/ReplServer.hs
+++ compiler/src/BrownPLT/Flapjax/ReplServer.hs
@@ -14,12 +14,14 @@ import Text.ParserCombinators.Parsec
 import Network.HTTP
 import Network.HTTP.Server
 import Network.URL
+import Network.BufferType
 import Network.URI
 import System.Directory
 import qualified Data.ByteString as BS
 import Data.ByteString (ByteString)
 import Data.ByteString.Internal (c2w, w2c)
 import qualified Data.List as L
+import qualified Codec.Binary.Base64.String as Base64
 
 parseExpr = do
   whiteSpace
@@ -60,24 +62,58 @@ compileExprService req = case (map w2c $ BS.unpack $ rqBody req) of
 flapjaxREPLServer :: Int -> FilePath -> IO ()
 flapjaxREPLServer port rootPath = server (handler rootPath)
 
+
+agentString = "Flapjax REPL Server (www.flapjax-lang.org)"
+
+--
+-- Derived from:
+-- http://hackage.haskell.org/packages/archive/HTTP/4000.0.8/
+--
+--
+
+toBufOps :: BufferType a => Request a -> BufferOp a
+toBufOps _ = bufferOps
+
+
+mkAuthRequest :: HStream ty => RequestMethod -> URI -> Request ty
+mkAuthRequest method uri = req
+  where req = Request uri method headers empty
+        basicHeaders = [ Header HdrContentLength "0"
+                       , Header HdrUserAgent agentString]
+        headers = case uriAuthority uri of
+          Just (URIAuth userInfo _ _) | length userInfo > 1 -> 
+            let auth = (head.tail) (L.tails userInfo) -- drops the trailing @
+                authBase64 = Base64.encode auth
+              in (Header HdrWWWAuthenticate authBase64):basicHeaders
+          otherwise -> basicHeaders
+        empty = buf_empty (toBufOps req)
+
+
+
+
 handler :: FilePath -> Handler ByteString
 handler rootPath sockAddr url request = do
   let path = uriPath (rqURI request)
-  putStrLn path
+  putStrLn $ "Servicing request: " ++ path
   case map tail (L.groupBy (const (/='/')) path) of
     ["compile"] -> compileExprService request
     ["ping"] -> do
       putStrLn "Responding to a ping."
       return (ok $ BS.pack $ map c2w "pong")
     ("redirect":path) -> do
-      let remoteURL = "http://" ++  concat (L.intersperse "/" path)
-      putStrLn remoteURL
+      let auth = case uriAuthority (rqURI request) of
+                   Just auth -> uriUserInfo auth
+                   Nothing -> ""
+      let remoteURL = uriScheme (rqURI request) ++ "//" ++ auth ++
+                      concat (L.intersperse "/" path)
       case parseURI remoteURL of
         Just uri -> do
-          let req = mkRequest GET uri
+          putStrLn $ "Proxying remote URL: " ++ show uri
+          let req = mkAuthRequest GET uri
+          putStrLn (show req)
           r <- simpleHTTP req
           case r of
-            Right rsp -> return rsp
+            Right rsp -> putStrLn (show rsp) >> return rsp
             Left err -> return four04 -- not really
         Nothing -> return four04 -- not really
     pathSegments -> do
