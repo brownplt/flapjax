3e68e692bb767796cf64f12f556f005d32677cfc
Arjun Guha <arjun.guha@gmail.com>
2011-09-08 20:15:02 -0700

    use native JSON processing

--- fx/flapjax-impl.js
+++ fx/flapjax-impl.js
@@ -1967,104 +1967,6 @@ var encodeREST = function(obj) {
   return str;
 };
 
-// From json.org
-var parseJSON = function(str) {
-  try {
-      return !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
-              str.replace(/"(\\.|[^"\\])*"/g, ''))) &&
-          eval('(' + str + ')');
-  } catch (e) {
-      throw 'cannot parse JSON string: ' + e;
-  }
-};
-
-var toJSONString = (function() {
-  var m = {
-          '\b': '\\b',
-          '\t': '\\t',
-          '\n': '\\n',
-          '\f': '\\f',
-          '\r': '\\r',
-          '"' : '\\"',
-          '\\': '\\\\'
-      };
-  var s = {
-    array: function (x) {
-      var a = ['['], b, f, i, l = x.length, v;
-      for (i = 0; i < l; i += 1) {
-        v = x[i];
-        f = s[typeof v];
-        if (f) {
-          v = f(v);
-          if (typeof v == 'string') {
-            if (b) {
-              a[a.length] = ',';
-            }
-            a[a.length] = v;
-            b = true;
-          }
-        }
-      }
-      a[a.length] = ']';
-      return a.join('');
-    },
-    'boolean': function (x) {
-      return String(x);
-    },
-    'null': function (x) {
-      return "null";
-    },
-    number: function (x) {
-      return isFinite(x) ? String(x) : 'null';
-    },
-    object: function (x) {
-      if (x) {
-        if (x instanceof Array) {
-          return s.array(x);
-        }
-        var a = ['{'], b, f, i, v;
-        for (i in x) {
-          v = x[i];
-          f = s[typeof v];
-          if (f) {
-            v = f(v);
-            if (typeof v == 'string') {
-              if (b) {
-                a[a.length] = ',';
-              }
-              a.push(s.string(i), ':', v);
-              b = true;
-            }
-          }
-        }
-        a[a.length] = '}';
-        return a.join('');
-      }
-      return 'null';
-    },
-    string: function (x) {
-      if (/["\\\x00-\x1f]/.test(x)) {
-        x = x.replace(/([\x00-\x1f\\"])/g, function(a, b) {
-            var c = m[b];
-            if (c) {
-              return c;
-            }
-            c = b.charCodeAt();
-            return '\\u00' +
-            Math.floor(c / 16).toString(16) +
-            (c % 16).toString(16);
-        });
-      }
-      return '"' + x + '"';
-    }
-  };
-  return function(val) {
-    var f = s[typeof val];
-    if (f) { return f(val); }
-    else { return undefined }
-  };
-})();
- 
 var getWebServiceObjectE = function(requestE) {
   var responseE = receiverE();
 
@@ -2079,7 +1981,7 @@ var getWebServiceObjectE = function(requestE) {
         body = '';
         method = 'GET';
       } else if (obj.request == 'post') {
-        body = toJSONString(obj.fields); 
+        body = JSON.stringify(obj.fields); 
         method = 'POST';
       } else if (obj.request == 'rawPost') {
         body = obj.body;
@@ -2101,7 +2003,7 @@ var getWebServiceObjectE = function(requestE) {
       if (obj.response == 'json') {
         xhr = ajaxRequest(method,url,body,async,
           function(xhr) {
-            responseE.sendEvent(parseJSON(xhr.responseText)); 
+            responseE.sendEvent(JSON.parse(xhr.responseText)); 
           });
       }
       else if (obj.response == 'xml') {
