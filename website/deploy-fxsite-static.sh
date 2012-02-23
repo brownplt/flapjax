61b33bc3861182fbfeb644f0716b01501acb76e0
Arjun Guha <arjun.guha@gmail.com>
2012-02-23 08:04:47 -0500

    moved website to its own repository

--- website/deploy-fxsite-static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ `hostname -d` = "cs.brown.edu" ]; then
-  rsync -rltovz --safe-links --delete build/ \
-    flapjax@flapjax-vista:/home/flapjax/web/static
-else
-  echo "ERROR: you must deploy from a cs.brown.edu machine."
-fi
